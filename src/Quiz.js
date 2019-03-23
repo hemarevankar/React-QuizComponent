11979ebb9f150db00416af58204ccd3c76544e90
Test Man <ttestman4@gmail.com>
2019-03-23 18:57:24 +0530

    modified the div

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react'
 
-//let quizData = require('./quiz_data.json')
+let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
-    // constructor(props) {
-    //     super(props)
-    //     this.state = {quiz_position:1}
-    // }  
+    constructor(props) {
+         super(props)
+         this.state = {quiz_position:1}
+    }  
     render() {
         return (
-            <div>Quiz</div>
+            <div><div className="QuizQuestion">
+            {quizData.quiz_Questions[0].instruction_text}
+            </div></div>
         )
     }
 }
